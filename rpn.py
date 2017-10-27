f0b35a26e52d1fb4e0c84559bf371ec01f7822cd
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2017-10-27 12:28:17 -0400

    Implement subtract

--- rpn.py
+++ rpn.py
@@ -8,9 +8,14 @@ def calculate(myarg):
             arg2 = stack.pop()
             result = arg1 + arg2
             stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
         else:
             stack.append(int(token))
-    print(stack)
+    #print(stack)
     return stack.pop()
 
 def main():
