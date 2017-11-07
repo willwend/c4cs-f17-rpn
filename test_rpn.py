bdd5a43ed777ec7702a3358ccb1450d486dab5ee
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2017-11-06 21:44:06 -0500

    Add exponentiation test

--- test_rpn.py
+++ test_rpn.py
@@ -11,3 +11,6 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('5 3 -')
         self.assertEqual(2, result)
 
+    def test_exp(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
