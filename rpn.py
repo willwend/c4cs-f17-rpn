a626fc868f1391e5e969048c8b2e02d70f023ede
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2017-11-08 08:49:07 -0500

    Add coloring

--- rpn.py
+++ rpn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import operator
+from clint.textui import colored
 
 ops = {
     '+': operator.add,
@@ -9,23 +10,32 @@ ops = {
     '^': operator.pow
 }
 
+def color(number):
+    if number < 0:
+        return colored.red(number)
+    else:
+        return colored.black(number)
+
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
         try:
             stack.append(int(token))
         except ValueError:
+            print(color(stack[0]) + ' ' + color(stack[1]) + ' ' + colored.blue(token))
             arg2 = stack.pop()
             arg1 = stack.pop()
             function = ops[token]
             result = function(arg1, arg2)
             stack.append(result)
-    #print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        result = calculate(raw_input("rpn calc> "))
+        print('Result: ' + color(result))
 
 if __name__ == '__main__':
     main()
