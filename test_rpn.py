9c697da436e8dd13d0211c85e2a11ae2ef99a27b
William Wendorf <williamwendorf@Williams-MacBook-Pro.local>
2017-11-08 17:57:23 -0500

    Final commit to show coverage change

--- test_rpn.py
+++ test_rpn.py
@@ -7,9 +7,21 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
 
-    def test_subtract(self):
+    def test_subt(self):
         result = rpn.calculate('5 3 -')
         self.assertEqual(2, result)
+
+    def test_mult(self):
+        result = rpn.calculate('7 3 -')
+        self.assertEqual(21, result)
+
+    def test_div(self):
+        result = rpn.calculate('8 2 /')
+        self.assertEqual(4, result)
+
+    def test_neg(self):
+        result = rpn.calculate('-4 -3 -')
+        self.assertEqual(-1, result)
 
     def test_exp(self):
         result = rpn.calculate('2 3 ^')
